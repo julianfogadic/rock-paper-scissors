eefb1bbbfb6aeb6df9c3dedd584f1b07e1420d47
julian <julian.fogadic@gmail.com>
2023-01-26 21:41:59 +0100

    Updated bug when returning winner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,18 +1,43 @@
-const options = ["Rock", "Paper", "Scissors"];
+//Making function to randomly return either rock, paper or scissors
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return(choices[randomChoice]);
+}
 
-//Making a function that plays round of rock paper scissors
-function playRound(playerSelection) {
-    const computerSelection = options[Math.floor(Math.random() * options.length)];
-    playerSelection = playerSelection.toLowerCase();
+//Making a function that play single round of Rock Paper Scissor
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+        return "Invalid selection. Please choose rock, paper or scissors";
+    }
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
-            playerSelection === "Scissors" && computerSelection === "Paper" ||
-            playerSelection === "Paper" && computerSelection === "Rock") {
-        return "You won! " + playerSelection + " beats " + computerSelection + ".";
-    } else {
-        return "You lose! " + computerSelection + " beats " + playerSelection + ".";
+        return "It's a tie";
+    }
+    if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return "You win! Rock beats Scissors!";
+        } else {
+            return "You lose! Paper beats Rock!";
+        }
+    }
+    if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats Rock!";
+        } else {
+            return "You lose! Scissors beats Paper!";
+        }
+    }
+    if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You win! Scissors beats paper!";
+        } else {
+            return "You lose! Rock beats Scissors!";
+        }
     }
 }
-const playerSelection = prompt("Please select rock, paper or scissors.")
-console.log(playRound(playerSelection));
+    
+// Using prompt function to get input from user
+const playerSelection = prompt("Please select either rock, paper or scissors.");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
