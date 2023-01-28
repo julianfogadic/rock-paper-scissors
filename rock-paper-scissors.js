864fac876a26139c229b379dc0d9f9d0453a09d9
julian <julian.fogadic@gmail.com>
2023-01-28 16:57:36 +0100

    Added loop to play game in 5 rounds, updated bugs

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,13 @@
+//Making function for computer to randomly selects rock, paper or scissors
+
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
+
+//Making function to check winner
 
 function checkWinner(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
@@ -21,19 +25,43 @@ function checkWinner(playerSelection, computerSelection) {
     }
 }
 
+//Making function to play a single round of rock, paper, scissors
+
 function playRound(playerSelection, computerSelection) {
     const result = checkWinner(playerSelection, computerSelection);
     if (result == "Tie") {
         return "It's a Tie!";
     }
     else if (result == "Player") {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
     }
     else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
     } 
 }
 
-const playerSelection = prompt("Please select rock, paper or scissors.");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+//Making game go to 5 rounds using loops
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 1; i <= 5; i++) {
+        const playerSelection = prompt("Please select rock, paper or scissors.");
+        const computerSelection = getComputerChoice();
+        const roundResult = playRound(playerSelection, computerSelection);
+        console.log(`Round ${i}: ${roundResult}`);
+        if (roundResult.includes("tie")) {
+            continue;
+        } else if (roundResult.includes("win")) {
+            playerScore++;
+        } else if (roundResult.includes("lose")) {
+            computerScore++;
+        }
+    }
+    if (playerScore > computerScore) {
+        return(`Well done! Player win ${playerScore}:${computerScore}`);
+    } else if (playerScore < computerScore) {
+        return(`Game over! Computer win ${playerScore}:${computerScore}`);
+    }
+}
+console.log(game(playRound));
